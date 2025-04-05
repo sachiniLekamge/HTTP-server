7eed55289b62a641a2157fd8788dd6f73ca9d538
IT20159030 <it20159030@my.sliit.lk>
2025-04-05 16:46:33 +0530

    Refactor request handling to improve structure and clarity

--- app/main.js
+++ app/main.js
@@ -1,57 +1,72 @@
-const fs = require("fs");
 const net = require("net");
-
+const fs = require("fs");
+// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
-const server = net.createServer((socket) => {
-  let requestBody = "";
-
-  socket.on("close", () => {
-    socket.end();
-  });
-
+const args = {};
+process.argv.forEach((arg, index) => {
+  if (arg.startsWith("--")) {
+    args[arg.replace(/^--/, "")] = process.argv[index + 1];
+  }
+});
+1;
+const FILES_DIR = args["directory"];
+// Uncomment this to pass the first stage
+const server = net.createServer({ keepAlive: true }, (socket) => {
   socket.on("data", (data) => {
-    const req = data.toString();
-    console.log(req);
-
-    const [method, path] = req.split("\r\n")[0].split(" ");
-
-    if (method === "GET" && path === "/") {
+    let path = data.toString().split(" ")[1];
+    if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (method === "GET" && path.startsWith("/files/")) {
-      const directory = process.argv[3];
-      const filename = path.split("/files/")[1];
-
-      if (fs.existsSync(`${directory}/${filename}`)) {
-        const content = fs.readFileSync(`${directory}/${filename}`).toString();
-        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
-        socket.write(res);
+    } else if (path.includes("/echo/")) {
+      let randomString = path.split("echo/")[1];
+      socket.write(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
+          randomString.length +
+          "\r\n\r\n" +
+          randomString
+      );
+    } else if (path.includes("user-agent")) {
+      let userAgent = data.toString().split("\r\n")[2].split(" ")[1];
+      console.log(userAgent);
+      socket.write(
+        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
+          userAgent.length +
+          "\r\n\r\n" +
+          userAgent
+      );
+    } else if (
+      path.startsWith("/files/") &&
+      data.toString().split(" ")[0] === "POST"
+    ) {
+      let fileName = path.split("/")[2];
+      const filePath = FILES_DIR + fileName;
+      const file = data.toString("utf-8").split("\r\n\r\n")[1];
+      fs.writeFileSync(filePath, file);
+      socket.write("HTTP/1.1 201 CREATED\r\n\r\n");
+    } else if (path.includes("/files/")) {
+      let fileName = path.split("/")[2];
+      console.log("FILENAME: " + fileName);
+      const filePath = FILES_DIR + fileName;
+      if (fs.existsSync(filePath)) {
+        const file = fs.readFileSync(filePath, "utf-8");
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`
+        );
       } else {
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        1;
       }
-    } else if (method === "POST" && path.startsWith("/files/")) {
-      const directory = process.argv[3];
-      const filename = path.split("/files/")[1];
-      const contentLengthLine = req
-        .split("\r\n")
-        .find((line) => line.startsWith("Content-Length:"));
-      const contentLength = parseInt(
-        contentLengthLine.split(":")[1].trim(),
-        10
-      );
-      requestBody = req.split("\r\n\r\n")[1].slice(0, contentLength);
-      fs.writeFileSync(`${directory}/${filename}`, requestBody);
-      const res = `HTTP/1.1 201 Created\r\n\r\n`;
-      socket.write(res);
     } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n");
     }
-
     socket.end();
+  });
+  socket.on("close", () => {
+    socket.end();
+    server.close();
   });
 });
 
 server.listen(4221, () => {
-  console.log("Server is listening on port 4221");
+  console.log("Server listening on port 4221");
   console.log("http://localhost:4221/");
 });
