fa97d2b97a2dbd621571492f2b90b0c94013917e
IT20159030 <it20159030@my.sliit.lk>
2025-04-05 12:52:55 +0530

    Comment out server.close() in socket close handler

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
-    server.close();
+    // server.close();
   });
 
   socket.on("data", (data) => {
