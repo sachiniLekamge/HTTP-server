a605c367abee65a6f066f87ada7e3f8cc6b3c758
IT20159030 <it20159030@my.sliit.lk>
2025-04-05 16:40:07 +0530

    Refactor request handling to separate GET and POST methods for improved clarity

--- app/main.js
+++ app/main.js
@@ -1,18 +1,25 @@
 const fs = require("fs");
 const net = require("net");
+
 console.log("Logs from your program will appear here!");
+
 const server = net.createServer((socket) => {
+  let requestBody = "";
+
   socket.on("close", () => {
     socket.end();
   });
+
   socket.on("data", (data) => {
     const req = data.toString();
     console.log(req);
-    const path = req.split(" ")[1];
-    if (path === "/") socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    else if (path.startsWith("/files/")) {
+
+    const [method, path] = req.split("\r\n")[0].split(" ");
+
+    if (method === "GET" && path.startsWith("/files/")) {
       const directory = process.argv[3];
       const filename = path.split("/files/")[1];
+
       if (fs.existsSync(`${directory}/${filename}`)) {
         const content = fs.readFileSync(`${directory}/${filename}`).toString();
         const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
@@ -20,24 +27,28 @@ const server = net.createServer((socket) => {
       } else {
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
       }
-    } else if (path === "/user-agent") {
-      req.split("\r\n").forEach((line) => {
-        if (line.includes("User-Agent")) {
-          const res = line.split(" ")[1];
-          socket.write(
-            `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n`
-          );
-        }
-      });
-    } else if (path.startsWith("/echo/")) {
-      const res = path.split("/echo/")[1];
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${res.length}\r\n\r\n${res}\r\n\r`
+    } else if (method === "POST" && path.startsWith("/files/")) {
+      const directory = process.argv[3];
+      const filename = path.split("/files/")[1];
+      const contentLengthLine = req
+        .split("\r\n")
+        .find((line) => line.startsWith("Content-Length:"));
+      const contentLength = parseInt(
+        contentLengthLine.split(":")[1].trim(),
+        10
       );
-    } else socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      requestBody = req.split("\r\n\r\n")[1].slice(0, contentLength);
+      fs.writeFileSync(`${directory}/${filename}`, requestBody);
+      const res = `HTTP/1.1 201 Created\r\n\r\n`;
+      socket.write(res);
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+
     socket.end();
   });
 });
+
 server.listen(4221, () => {
   console.log("Server is listening on port 4221");
   console.log("http://localhost:4221/");
