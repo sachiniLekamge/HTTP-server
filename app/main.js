b5995812f67f3a5321a030e0acc377f0b31f5919
IT20159030 <it20159030@my.sliit.lk>
2025-04-05 12:55:15 +0530

    Refactor socket data handling to improve file request processing and remove unused code

--- app/main.js
+++ app/main.js
@@ -1,49 +1,61 @@
 const net = require("net");
+const fs = require("fs");
+const path = require("path");
+
+const directory = process.argv[3];
+
+if (!directory) {
+  console.error(
+    "Error: Please provide a directory path using --directory flag."
+  );
+  process.exit(1);
+}
 
 console.log("Logs from your program will appear here!");
+console.log("Serving files from:", directory);
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
-    // server.close();
   });
 
   socket.on("data", (data) => {
     const request = data.toString();
-    const path = request.split(" ")[1];
+    const pathRequested = request.split(" ")[1];
     let responseStatus = "404 Not Found";
     let responseBody = "";
     let contentType = "text/plain";
     let contentLength = 0;
 
-    const echoMatch = path.match(/^\/echo\/(.+)$/);
-    if (path === "/" || echoMatch) {
-      responseStatus = "200 OK";
-      if (echoMatch) {
-        responseBody = echoMatch[1];
-        contentLength = responseBody.length;
-      }
-    }
+    if (pathRequested.startsWith("/files/")) {
+      const filename = pathRequested.slice(7);
+      const filePath = path.join(directory, filename);
 
-    const userAgentMatch = path === "/user-agent";
-    if (userAgentMatch) {
-      const userAgentHeader = request
-        .split("\r\n")
-        .find((line) => line.startsWith("User-Agent:"));
-      if (userAgentHeader) {
-        responseStatus = "200 OK";
-        responseBody = userAgentHeader.split(": ")[1];
-        contentLength = responseBody.length;
-      }
-    }
+      fs.stat(filePath, (err, stats) => {
+        if (err || !stats.isFile()) {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        } else {
+          fs.readFile(filePath, (err, data) => {
+            if (err) {
+              socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            } else {
+              responseStatus = "200 OK";
+              responseBody = data.toString();
+              contentLength = stats.size;
+              contentType = "application/octet-stream";
 
-    socket.write(`HTTP/1.1 ${responseStatus}\r\n`);
-    socket.write(`Content-Type: ${contentType}\r\n`);
-    socket.write(`Content-Length: ${contentLength}\r\n`);
-    socket.write(`\r\n`);
+              socket.write(`HTTP/1.1 ${responseStatus}\r\n`);
+              socket.write(`Content-Type: ${contentType}\r\n`);
+              socket.write(`Content-Length: ${contentLength}\r\n`);
+              socket.write(`\r\n`);
 
-    if (responseStatus === "200 OK") {
-      socket.write(responseBody);
+              socket.write(data);
+            }
+          });
+        }
+      });
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
   });
 });
