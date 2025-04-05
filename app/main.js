01116aace134202d0c55765623b82cef49c2a60a
IT20159030 <it20159030@my.sliit.lk>
2025-04-05 12:35:30 +0530

    Refactor server creation and response handling in main.js

--- app/main.js
+++ app/main.js
@@ -1,14 +1,20 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
+    server.close();
+  });
+  socket.on("data", (data) => {
+    const path = data.toString().split(" ")[1];
+    const responseStatus = path === "/" ? "200 OK" : "404 Not Found";
+    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, () => {
+  console.log("Server is listening on port 4221");
+  console.log("http://localhost:4221/");
+});
